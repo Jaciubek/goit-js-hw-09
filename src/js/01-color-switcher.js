99fa3f061b4f92edbb85cf26918c5571668a6e26
Jaciubek <>
2022-07-02 19:35:45 +0200

    add 01color and 02timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+// zad dom 9
+// 01-color-switcher.js
 
+const startBtn = document.querySelector('[data-start]');
+
+const stopBtn = document.querySelector('[data-stop');
+// function to random change color
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// click on and startBtn event
+startBtn.addEventListener('click', handleEvent);
+let timerId = null;
+function handleEvent() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+// click on and stopBtn event
+stopBtn.addEventListener('click', stopEvent);
+function stopEvent() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
